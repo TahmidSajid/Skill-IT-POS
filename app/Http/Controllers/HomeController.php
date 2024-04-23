f1e535e0feba3f029499edab8d6bdf4549044e33
TahmidSajid <tahmidkhansajid@gmail.com>
2024-04-24 02:17:45 +0600

    Dashboard complete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Courses;
+use App\Models\Enrollments;
+use App\Models\Payments;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -24,6 +27,38 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        $costs = Courses::select('cost')->get();
+        $total_cost = 0;
+        foreach ($costs as $key => $cost) {
+            $total_cost = $cost->cost + $total_cost;
+        }
+
+        $paid_payments = Payments::select('payment')->where('status','paid')->get();
+        $total_paid = 0 ;
+        foreach ($paid_payments as $key => $payment) {
+            $total_paid = $payment->payment + $total_paid;
+        }
+
+        $due_payments = Payments::select('payment')->where('status','unpaid')->get();
+        $total_due = 0 ;
+        foreach ($due_payments as $key => $payment) {
+            $total_due = $payment->payment + $total_due;
+        }
+
+
+        $total_loss = 0;
+        $total_gain = 0;
+        if ($total_cost>$total_paid) {
+            $total_loss = $total_cost-$total_paid;
+        }
+        else{
+            $total_gain = $total_paid-$total_cost;
+        }
+
+        $total_student = User::where('role','student')->count();
+        $active_student = User::where('role','student')->where('status','active')->count();
+        $enrolled_student = Enrollments::select('user_id')->distinct()->count();
+        $launched_course = Courses::count();
+        return view('dashboard',compact('total_cost','total_paid','total_loss','total_gain','total_due','total_student','active_student','enrolled_student','launched_course'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,125 @@
 @extends('layouts.dashboard')
 @section('content')
-
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="dash-widget dash2">
+                <div class="dash-widgetimg">
+                    <span>
+                        <img src="assets/img/icons/dash2.svg" alt="img" />
+                    </span>
+                </div>
+                <div class="dash-widgetcontent">
+                    <h5>
+                        ৳<span class="counters" data-count="{{ $total_cost }}"></span>
+                    </h5>
+                    <h6>Total Cost</h6>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="dash-widget dash2">
+                <div class="dash-widgetimg">
+                    <span>
+                        <img src="assets/img/icons/dash2.svg" alt="img" />
+                    </span>
+                </div>
+                <div class="dash-widgetcontent">
+                    <h5>
+                        ৳<span class="counters" data-count="{{ $total_paid }}"></span>
+                    </h5>
+                    <h6>Total Money Got</h6>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="dash-widget dash2">
+                <div class="dash-widgetimg">
+                    <span>
+                        <img src="assets/img/icons/dash2.svg" alt="img" />
+                    </span>
+                </div>
+                <div class="dash-widgetcontent">
+                    <h5>
+                        ৳<span class="counters" data-count="{{ $total_due }}"></span>
+                    </h5>
+                    <h6>Total Due</h6>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="dash-widget dash2">
+                <div class="dash-widgetimg">
+                    <span>
+                        @if ($total_loss > 0)
+                            <img src="assets/img/icons/dash3.svg" alt="img" />
+                        @else
+                            <img src="assets/img/icons/dash4.svg" alt="img" />
+                        @endif
+                    </span>
+                </div>
+                <div class="dash-widgetcontent">
+                    @if ($total_loss > 0)
+                        <h5>
+                            ৳<span class="counters" data-count="{{ $total_loss }}"></span>
+                        </h5>
+                    @else
+                        <h5>
+                            ৳<span class="counters" data-count="{{ $total_gain }}"></span>
+                        </h5>
+                    @endif
+                    @if ($total_loss > 0)
+                        <h6>Total Money Lost</h6>
+                    @else
+                        <h6>Total Money Gain</h6>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-3">
+            <div class="dash-count">
+                <div class="dash-counts">
+                    <h4>{{ $total_student }}</h4>
+                    <h5>Total Student</h5>
+                </div>
+                <div class="dash-imgs">
+                    <i data-feather="user"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="dash-count das1">
+                <div class="dash-counts">
+                    <h4>{{ $active_student }}</h4>
+                    <h5>Total Active Student</h5>
+                </div>
+                <div class="dash-imgs">
+                    <i data-feather="user-check"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="dash-count das3">
+                <div class="dash-counts">
+                    <h4>{{ $enrolled_student }}</h4>
+                    <h5>Total Enrolled Student</h5>
+                </div>
+                <div class="dash-imgs">
+                    <i data-feather="user-check"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="dash-count das2">
+                <div class="dash-counts">
+                    <h4>{{ $launched_course }}</h4>
+                    <h5>Total Launched Course</h5>
+                </div>
+                <div class="dash-imgs">
+                    <i data-feather="file"></i>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
